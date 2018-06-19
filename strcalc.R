c17b3918f5ce9db61063ae1209f43e57eeff3e2d
Raynes, Jared (A&F, Werribee SnydRd) <Jared.Raynes@csiro.au>
2018-06-19 12:33:08 +1000

    added division

--- strcalc.R
+++ strcalc.R
@@ -8,7 +8,7 @@ compute = function(input_string) {
   num1 = as.integer(values[3])
   result = switch(operator,
                   '+' = num0 + num1,
-                  '-' = num0 - num1,NA
+                  '/' = num0 - num1,NA
   if (is.na(result)) {
     print('unknown operator!')
   })
